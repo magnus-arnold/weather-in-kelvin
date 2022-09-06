0e986861b7372a220ec17b07a30c8aad0f5f1ede
David Arnold <d@0x1.org>
2022-09-06 13:45:42 +0000

    Restart uWSGI after upload.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-scp -p app.py search.svg site.css site.html site.js root@services.0x1.org:/var/www/weatherinkelvin-com/
+WEBHOST=services.0x1.org
+
+scp -p app.py search.svg site.css site.html site.js root@$WEBHOST:/var/www/weatherinkelvin-com/
+ssh root@$WEBHOST /etc/init.d/uwsgi restart
